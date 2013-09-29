a90af91a5f8ad86e6eb3c30a2e2d06a57fa22b3f
Andreas Haller <andreashaller@gmail.com>
2013-09-29 02:48:35 +0200

    Show gravatar image in tracker snippet. TODO Add fallback

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,7 @@
 module ApplicationHelper
+  def gravatar_url_for(email, options = { size: 40 })
+    size = options[:size]
+    gravatar_id = Digest::MD5::hexdigest(email.downcase)
+    gravatar_url = "https://secure.gravatar.com/avatar/#{gravatar_id}?s=#{size}"
+  end
 end
